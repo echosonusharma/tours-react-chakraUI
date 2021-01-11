f0dc734f2512a0991ddb79bc356b9383490c2a71
Sonu Sharma <echosonusharma@gmail.com>
2021-01-12 00:43:49 +0530

    skeleton not working

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import Tours from './components/Tours';
 import Loading from './components/Loading';
-import { Box, Heading, Container, Button } from "@chakra-ui/react";
+import { Box, Heading, Container, Center, Button } from "@chakra-ui/react";
 
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
-
+  const [loading, setLoading] = useState(true)
   const [places, setPlaces] = useState([]);
 
-  const getData = async () => {
+  const removeTour = (id) => {
+    const newTourPlaces = places.filter((tour) => tour.id !== id)
+    setPlaces(newTourPlaces)
+  }
+
+  const fetchTourData = async () => {
     const res = await fetch(url);
     const data = await res.json();
+    setLoading(false)
     setPlaces(data);
   }
 
   useEffect(() => {
-    getData();
+    fetchTourData();
   }, []);
 
-
+  if (loading) {
+    return (
+      <main>
+        <Loading places={places} pt='10rem' />
+      </main>
+    )
+  }
+  if (places.length === 0) {
+    return (
+      <main>
+        <Box>
+          <Center><Heading mb={4} pt={36} >No tours left</Heading></Center>
+          <Center><Button size="lg" colorScheme="green" mt="24px"
+            onClick={() => fetchTourData()}>Refresh</Button></Center>
+        </Box>
+      </main>
+    )
+  }
   return (
     <>
       <Container maxW="3xl" centerContent>
         <Heading as="h1" pt='2rem' >Our Tours</Heading>
         <Box bg="green.400" w="7rem" h='0.5rem' mb="5rem"></Box>
-        <Button >Color Mode</Button>
-        <Tours places={places} />
+        <Tours places={places} removeTour={removeTour} />
       </Container>
-      <Loading places={places} />
-
     </>
   )
 }
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Box, Image, Text, Grid, Button, Center } from "@chakra-ui/react";
+import { Box, Image, Text, Flex, Button, Center, useToast } from "@chakra-ui/react";
+import { WarningIcon } from '@chakra-ui/icons';
 
-const toursContainer = ({ places }) => {
+const toursContainer = ({ places, removeTour }) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const toast = useToast()
     return (
         <>
             {places.map((place) => {
@@ -10,12 +13,29 @@ const toursContainer = ({ places }) => {
                     <Box key={id} pb='5rem' onClick={() => console.log('click')} >
                         <Box border="3px" borderColor="gray.300" borderRadius='0.2rem' boxShadow="md" pb={2}>
                             <Image src={image} alt={name} pb={3} width='100%' height='30rem' objectFit='cover' />
-                            <Grid templateColumns="repeat(2, 1fr)" >
+                            <Flex direction="row" justifyContent="space-between">
                                 <Box fontWeight="semibold" as="h4" textAlign='start' pl={2}> {name} </Box>
                                 <Box textAlign='end' pr={2} color='green.400' > ${price}</Box>
-                            </Grid>
+                            </Flex>
                             <Text fontSize="ml" p={2} pt={2} pb={2}>{info}</Text>
-                            <Center><Button color='red.400' >Not Interested</Button></Center>
+                            <Center><Button
+                                color='red.400'
+                                onClick={() => {
+                                    removeTour(id);
+                                    toast({
+                                        position: "top",
+                                        duration: 2000,
+                                        render: () => (
+                                            <Box color="white" p={4} bg="red.400" borderRadius='md'>
+                                                <Flex direction="row" justifyContent="space-between" >
+                                                    <WarningIcon w={6} h={6} />
+                                                    <Box pl={3}>{name} has been removed !</Box>
+                                                </Flex>
+                                            </Box>
+                                        ),
+                                    })
+                                }
+                                } >Not Interested</Button></Center>
                         </Box>
                     </Box>
                 );
